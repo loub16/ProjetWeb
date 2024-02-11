9f45f71fb9105cd799a57fc50cc7bbfa221d240b
YConanec <yconanec@etud.univ-angers.fr>
2024-02-11 17:04:22 +0100

    wip trajet

--- scripts/trajet.js
+++ scripts/trajet.js
@@ -4,6 +4,8 @@ import * as fs from 'fs';
 import AdmZip from "adm-zip";
 import path from 'path';
 import * as csvToJson from "convert-csv-to-json";
+import e from "express";
+import { start } from "repl";
 
 const decalage30 = 1800000
 /**json contenant les info liées aux horraires des arrêt */
@@ -16,6 +18,7 @@ const dataRoute = await csvToJson.fieldDelimiter(',').getJsonFromCsv("donnees\\t
 const dataStops = await csvToJson.fieldDelimiter(',').getJsonFromCsv("donnees\\trajet_static\\stops.csv");
 /**modèle de date pour ne comparer que les différences d'heures et pas de date,  je json fournissant datastop time ne fournissant que des heures*/
 const hourstring = "2023-10-10 "
+
 
 
 
@@ -36,7 +39,7 @@ export async function getTransportAt(arret, datedepart, nbParligne) {
   const date30 = new Date(new Date(date).getTime() + decalage30)
   initTrajet()
   var transports = {};
- if (datedepart.getTime() < date30) {
+  if (datedepart.getTime() < date30) {
     console.log("RT")
 
     transports = await getTransportAtRT(arret, nbParligne)
@@ -97,12 +100,12 @@ async function getTransportAtRT(arret, nbParligne) {
       if (entity.tripUpdate) {
         entity.tripUpdate.stopTimeUpdate.forEach((TimeUpdate) => {
           if (TimeUpdate.stopId === arret && TimeUpdate.arrival.time.low * 1000 > datenow) {
-            ent=entity
+            ent = entity
             arrivalTime = new Date(TimeUpdate.arrival.time.low * 1000).toLocaleTimeString()
             const [headsign, idLigne] = getInfoTrip(dataTrip, entity.tripUpdate.trip.tripId);
             const [route_name, route_color] = getInfoRoute(dataRoute, entity.tripUpdate.trip.routeId)
-            dict[entity.tripUpdate.trip.tripId] = { routeId: entity.tripUpdate.trip.routeId, routeName: route_name, headsign: headsign,arrival: arrivalTime, color: route_color };
-          
+            dict[entity.tripUpdate.trip.tripId] = { routeId: entity.tripUpdate.trip.routeId, routeName: route_name, headsign: headsign, arrival: arrivalTime, color: route_color };
+
           }
         })
       }
@@ -176,12 +179,8 @@ async function getTransportAtStatic(arret, heureDepart, nbParligne) {
  * @returns {array} informations de la ligne [headsign, id]
  */
 function getInfoTrip(data, idTrip) {
-  console.log(idTrip)
-  
   for (let entity of data) {
-
     if (entity.trip_id === idTrip) {
-      console.log("entité",entity)
       return [entity.trip_headsign, entity.route_id];
     }
   }
@@ -271,6 +270,12 @@ function extratXperLigne(data, nbtrajet) {
   return dict
 }
 
+/**
+ * Récupère les identifiants des arrêts pour un nom d'arrêt donné à partir des données fournies.
+ * @param {Array} dataStops - Le tableau des données d'arrêt.
+ * @param {string} nomArret - Le nom de l'arrêt à rechercher.
+ * @returns {Array} - Un tableau d'identifiants d'arrêt correspondant au nom d'arrêt donné.
+ */
 function getArret(dataStops, nomArret) {
   const stopIds = [];
   for (const stop of dataStops) {
@@ -281,4 +286,188 @@ function getArret(dataStops, nomArret) {
   return stopIds;
 }
 
+function getTrajet(trajets, destinations) {
 
+
+}
+
+/**
+ * Récupère une liste d'identifiants d'arrêts pour un ID de trajet donné.
+ * @param {string} trip_id - L'ID du trajet.
+ * @returns {Array<string>} - Un tableau d'identifiants d'arrêts.
+ */
+function getListArretStatic(trip_id) {
+  return dataStopTime.filter(entity => entity.trip_id === trip_id).map(entity => entity.stop_id);
+}
+
+/**
+ * Vérifie si un arrêt fait partie d'un trajet.
+ * @param {string} trip_id - L'identifiant du trajet.
+ * @param {string} arret - L'arrêt à vérifier.
+ * @returns {boolean} - True si l'arrêt fait partie du trajet, sinon False.
+ */
+function isArretOnTripStatic(trip_id, arret) {
+  return getListArretStatic(trip_id).includes(arret);
+}
+
+/**
+ * Récupère l'heure d'arrivée d'un arrêt sur un trajet.
+ * @param {string} trip_id - L'identifiant du trajet.
+ * @param {string} arret - L'arrêt.
+ * @returns {string} - L'heure d'arrivée de l'arrêt sur le trajet.
+ */
+function getHeurearriveStaticDirecte(trip_id, arret) {
+  if (!isArretOnTripStatic(trip_id, arret)) {
+    return null;
+  }
+  const entity = dataStopTime.find(entity => entity.trip_id === trip_id && entity.stop_id === arret);
+  return entity.arrival_time;
+}
+function getHeurearriveStatic(trip_id, arret) {
+  if (isArretOnTripStatic(trip_id, arret)) {
+    return getHeurearriveStaticDirecte(trip_id, arret);
+  }
+}
+function getArretWithEscale(trip_id, arretfinal) {
+  const arrets = getListArretStatic(trip_id);
+  const routes = [];
+
+  for (const route of dataRoute) {
+    if (route.route_id !== trip_id) {
+      const routeArrets = getListArretStatic(route.route_id);
+      if (routeArrets.includes(arretfinal)) {
+        for (const arret of arrets) {
+          if (routeArrets.includes(arret)) {
+            routes.push(route.route_id);
+            break;
+          }
+        }
+      }
+    }
+  }
+  return routes;
+}
+
+
+/**
+ * Récupère les trajets avec les arrêts communs.
+ * 
+ * @param {string} tripIdDepart - L'ID du trajet de départ.
+ * @param {string} arretfinal - L'arrêt final.
+ * @returns {Array} - Un tableau de trajets avec des arrêts communs. de la forme [tripId, [arretCommun, heureArrivee, heureDepart]]
+ */
+function getTripsWithArrets(tripIdDepart, arretfinal) {
+  const start = new Date();
+  const trips = [];
+  const arretsDepart = getListArretStatic(tripIdDepart);
+  const arretsFinal = getListArretStatic(arretfinal);
+
+  for (const trip of dataTrip) {
+    const tripId = trip.trip_id;
+    const stops = getListArretStatic(tripId);
+
+    if (stops.includes(arretfinal) && stops.some(stop => arretsDepart.includes(stop))) {
+      const commonArrets = stops.filter(stop => arretsDepart.includes(stop));
+      const heureArriveeTrip1 = heureToDateTime(getHeureArrivee(tripIdDepart, commonArrets[0]));
+      const heureDepartTrip2 = heureToDateTime(getHeureDepart(tripId, commonArrets[0]));
+      for (const arret of commonArrets) {
+        
+        
+
+        if (heureDepartTrip2.getTime() > heureArriveeTrip1.getTime() && heureDepartTrip2.getTime() < heureArriveeTrip1.getTime() + decalage30) {
+                    trips.push([tripId, getCommonArret(tripIdDepart, tripId)]);
+          break; // Exit the loop when a trip is found
+        }
+      }
+    }
+  }
+  const end = new Date() - start;
+  console.log(end);
+  return trips;
+}
+var correspondance=(getTripsWithArrets("5369904", "SOLELEVA"))
+console.log(correspondance)
+console.log("fastest")
+var fastest=getFastestCorrespondance(correspondance)
+console.log(fastest)
+console.log("least wait time")
+var leastWaitTime=getLeastWaitTime(fastest[1])
+console.log(leastWaitTime)
+
+
+/**
+ * Retourne un tableau des arrêts communs entre deux trajets.
+ *
+ * @param {string} trip_id1 - L'ID du premier trajet.
+ * @param {string} trip_id2 - L'ID du deuxième trajet.
+ * @returns {Array} - Un tableau des arrêts communs entre les deux trajets.
+ */
+function getCommonArret(trip_id1, trip_id2) {
+  const arrets1 = getListArretStatic(trip_id1);
+  const arrets2 = getListArretStatic(trip_id2);
+  const commonArrets = arrets1.filter(arret => arrets2.includes(arret));
+  const vRet = []
+  commonArrets.forEach(arret => {
+    const heureArrivee = getHeureArrivee(trip_id1, arret);
+    const heureDepart = getHeureDepart(trip_id2, arret);
+    vRet.push([arret, heureArrivee, heureDepart]);
+  })
+  return vRet;
+}
+
+/**
+ * Récupère l'heure d'arrivée d'un trajet à un arrêt spécifique.
+ * @param {string} trip_id - L'identifiant du trajet.
+ * @param {string} arret - L'identifiant de l'arrêt.
+ * @returns {Array<string>} - Un tableau contenant les heures d'arrivée correspondantes.
+ */
+function getHeureArrivee(trip_id, arret) {
+  return dataStopTime
+    .filter(entity => entity.trip_id === trip_id && entity.stop_id === arret)
+    .map(entity => entity.arrival_time);
+}
+
+/**
+ * Récupère l'heure de départ pour un trajet et un arrêt donnés.
+ * @param {string} trip_id - L'ID du trajet.
+ * @param {string} arret - L'ID de l'arrêt.
+ * @returns {Array<string>} - Un tableau d'heures de départ.
+ */
+function getHeureDepart(trip_id, arret) {
+  return dataStopTime
+    .filter(entity => entity.trip_id === trip_id && entity.stop_id === arret)
+    .map(entity => entity.departure_time);
+}
+
+/**
+ * Convertit une chaîne de caractères représentant une heure en un objet Date JavaScript.
+ * @param {string} heure - La chaîne de caractères représentant l'heure.
+ * @returns {Date} - L'objet Date correspondant à l'heure spécifiée.
+ */
+
+function heureToDateTime(heure) {
+  return new Date(hourstring + heure);
+}
+function getFastestCorrespondance(correspondance){
+  var fastest = correspondance[0]
+  for (const corresp of correspondance) {
+    if (heureToDateTime(corresp[1][2][2]) < heureToDateTime(fastest[1][2][2])) {
+      fastest = corresp
+    }
+  }
+
+  return fastest
+}
+function getLeastWaitTime(Arraylist){
+  
+  var durée=heureToDateTime(Arraylist[0][2])-heureToDateTime(Arraylist[0][1])
+
+  var fastest=Arraylist[0]
+  for (const arret of Arraylist) {
+    if (heureToDateTime(arret[2])-heureToDateTime(arret[1])<durée){
+      durée=heureToDateTime(arret[2])-heureToDateTime(arret[1])
+      fastest=arret[0]
+    }
+  }
+    return fastest
+  }
