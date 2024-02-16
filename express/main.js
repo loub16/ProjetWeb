5ff2ba91f81c11d80ad8d159cfcf497e23f10f74
Pierre Portron <pportron@etud.univ-angers.fr>
2024-02-16 12:16:00 +0100

    change date in fetch transport

--- express/main.js
+++ express/main.js
@@ -1,6 +1,13 @@
+ //Variables
+ var selectedDate = new Date();
 document.addEventListener('DOMContentLoaded', async () => {
-    // Get the user ID from the local storage
-    const id = localStorage.getItem("userId");
+    //set the minimum date of the calendar to the current date
+    const currentDate = new Date().toISOString().split('T')[0];
+    // Set the minimum date for the calendar input
+    document.getElementById('calendar').setAttribute('min', currentDate);
+    // Get the id of the student from the url
+    const searchParams = new URLSearchParams(window.location.search);
+    const id = searchParams.get('id');
     // Get the timetable of the student
     const [firstday, lastday] = getFirstAndLastDayOfCurrentWeek();
     const edt = await fetchEdt(id, firstday, lastday);
@@ -25,19 +32,7 @@ document.addEventListener('DOMContentLoaded', async () => {
             const lastLessonHour = getLastLessonHour(edt, rearrangedDate);
             document.getElementById('heureCours').textContent = lastLessonHour;
             document.getElementById('date').textContent = formatDate(new Date(selectedDate));
-
-            // Actulaize the transport hour on change of the date
-            const sensB = document.getElementById('sensB');
-            const sensT = document.getElementById('sensT');
-            if(sensB != null){
-                setTransportHour(sensB.value);
-            }
-            if(sensT != null){
-                setTransportHour(sensT.value);
-            }
-            
-
-        })();   
+        })();    
     });
 });
 
@@ -141,20 +136,22 @@ function getFormattedDate() {
 }
 
 /** Convert a date in a timesamp
- * @param {string} date date to convert in format HH:MM:SS
+ * @param {string} date date to convert in format YYYY-MM-DD
+ * @param {string} time time to convert in format HH:MM:SS
  * @returns timestamp (int)
  */
-function dateToTimestamp(date) {
-    const currentDate = new Date();
-    dateSplit = date.split(':');
-
-    currentDate.setHours(dateSplit[0]);
-    currentDate.setMinutes(dateSplit[1]);
-    currentDate.setSeconds(dateSplit[2]);
-    currentDate.setMilliseconds(0);
-
+function dateToTimestamp(date,time) {
+    const dateTime = new Date(date);
+    console.log("date avant time: " + dateTime)
+    timeSplit = time.split(':');
+    dateTime.setHours(timeSplit[0]);
+    dateTime.setMinutes(timeSplit[1]);
+    dateTime.setSeconds(timeSplit[2]);
+    dateTime.setMilliseconds(0);
+    console.log("date avec time: " + dateTime)
     // Get the timestamp in milliseconds since the Unix epoch (January 1, 1970 00:00:00 UTC)
-    const timestamp = currentDate.getTime();
+    const timestamp = dateTime.getTime();
+    console.log("timestamp: " + timestamp)
     return timestamp;
 }
 
@@ -166,7 +163,7 @@ function dateToTimestamp(date) {
 async function fetchTransport(arret, lessonHour){
     try {
         //arret = (transport === "bus") ? "NDAMELAC" : "1BEAU";
-        const response = await fetch('http://localhost:3000/getTransport?arret=' + arret + '&heure=' + dateToTimestamp(lessonHour));
+        const response = await fetch('http://localhost:3000/getTransport?arret=' + arret + '&heure=' + dateToTimestamp(selectedDate,lessonHour));
         const bus = await response.json();
         return bus;
     }catch (error) {
@@ -296,25 +293,21 @@ async function setTransportHour(sens){
         const busHour = getBusHour(bus);
         const arretHour = sens == 'SB' ? busHour[0] : busHour[1];
         document.getElementById('heureBus').textContent = arretHour;
-        document.getElementById('text1').removeAttribute("hidden");
     }else if(sens == 'BS' || sens == 'AB'){
         const bus = await fetchTransport("NDAMLA-E",document.getElementById('heureCours').textContent);
         const busHour = getBusHour(bus);
         const arretHour = sens == 'BS' ? busHour[0] : busHour[1];
         document.getElementById('heureBus').textContent = arretHour;
-        document.getElementById('text1').removeAttribute("hidden");
     }else if(sens == 'BR' || sens == 'BM'){
         const tram = await fetchTransport("1BEAU",document.getElementById('heureCours').textContent);
         const tramHour = getTramHour(tram);
         const arretHour = sens == 'BM' ? tramHour[0] : tramHour[1];
         document.getElementById('heureTram').textContent = arretHour;
-        document.getElementById('text2').removeAttribute("hidden");
     }else if(sens == 'RB' || sens == 'MB'){
         const tram = await fetchTransport("2BEAU",document.getElementById('heureCours').textContent);
         const tramHour = getTramHour(tram);
         const arretHour = sens == 'MB' ? tramHour[0] : tramHour[1];
         document.getElementById('heureTram').textContent = arretHour;
-        document.getElementById('text2').removeAttribute("hidden");
     }
     
 }
